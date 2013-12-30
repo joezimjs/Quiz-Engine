79bd61c20a0b5dc40ce15bd4fef871619a1401e2
Joseph Zimmerman <contact@joezimjs.com>
2013-12-30 15:13:15 -0600

    More minor tweaks

--- js/modules/Data/quizzes.js
+++ js/modules/Data/quizzes.js
@@ -1,7 +1,7 @@
 QuizEngine.module('Data', function(Data) {
 	
 	Data.addInitializer(function(options) {
-		// We do not have any initial quiz data, but if we do have it in the future, this is already ready
+		// Grab the data from the option passed in to the App's/Module's `start` method
 		Data.quizzes = new Data.Quizzes((options && options.data && options.data.quizzes) || []);
 	});
 
