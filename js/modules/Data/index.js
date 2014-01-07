bbdd0317b70117162955cf342782d80de1445465
Joseph Zimmerman <contact@joezimjs.com>
2014-01-06 22:01:27 -0600

    Moved the Data module initialization all into index.js. There was no really good reason to keep it separate.

--- js/modules/Data/index.js
+++ js/modules/Data/index.js
@@ -1,6 +1,14 @@
 QuizEngine.module('Data', function(Data) {
     
-    // Nothing needed to be done here
-    // Leaving it open just in case
+    Data.addInitializer(function(options) {
+        // Grab the data from the option passed in to the App's/Module's `start` method
+        Data.questions = new Data.Categories((options && options.data && options.data.questions) || []);
+        Data.quizzes = new Data.Quizzes((options && options.data && options.data.quizzes) || []);
+    });
+
+    Data.addFinalizer(function() {
+        delete Data.questions;
+        delete Data.quizzes;
+    });
 
 });
